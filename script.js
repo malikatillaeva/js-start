ecb4263f0b7dd90e4f0b4881de38a1df2c9b7492
Tillaeva <tillaevajm@gamil.com>
2021-01-15 18:20:54 +0500

    work

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Hello")
+console.log(20)
+console.log(true)
+
+
+var perem = 'slovo'
+
+console.log(perem);
+
+// yog 200 tuz 300 shakar 400
+var yog = 250, tuz = 300, shakar = 400
+var sum1 = yog + tuz
+var sum2 = tuz + shakar
+
+var name ='ali'
+var age = 'yigirma'
+
+var umumiy = name + " " +age
+
+console.log(sum1, sum2);
+
+console.log(umumiy);
+
+// ++increment
+// --dicrement
+// ++i prefiksatsiya, i++ postfiksatsiya
+var i=0
+console.log(i--);
+console.log(i);
+
+var gdeto = prompt('hello world')
+console.log(gdeto);
+
+alert('cerniy plash')
